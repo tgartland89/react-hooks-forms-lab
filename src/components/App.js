2ea649be0b45befabc5a821db652a115f95959c0
tgartland89 <thomas.gartland@gmail.com>
2023-05-23 22:23:29 -0600

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,21 @@ import Header from "./Header";
 import itemData from "../data/items";
 
 function App() {
-  const [items, setItems] = useState(itemData);
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  let [items, setItems] = useState(itemData);
+  let [isDarkMode, setIsDarkMode] = useState(false);
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function handleItemFormSubmit(newItem) {
+    setItems([...items, newItem]);
+  }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={handleItemFormSubmit} />
     </div>
   );
 }
